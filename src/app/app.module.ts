515e954e2c7281fecbca6f24cbf3259a8f7961e7
Santiago Encarnacion <santiago.503.es@gmail.com>
2022-07-21 09:25:25 -0400

    added: structure of folders, styles bg and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './page/welcome/welcome.component';
+import { MaterialModule } from 'src/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
